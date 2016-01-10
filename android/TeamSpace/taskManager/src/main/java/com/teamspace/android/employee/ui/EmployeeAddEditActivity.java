8a7c80a3788c29c8c852f399ae9f3338109d398b
TeamSpaceDev <teamspacedev@gmail.com>
2016-01-10 12:31:24 -0800

    Fix bug with employee edit screen

--- android/TeamSpace/taskManager/src/main/java/com/teamspace/android/employee/ui/EmployeeAddEditActivity.java
+++ android/TeamSpace/taskManager/src/main/java/com/teamspace/android/employee/ui/EmployeeAddEditActivity.java
@@ -1,6 +1,7 @@
 package com.teamspace.android.employee.ui;
 
 import com.teamspace.android.common.ui.SingleFragmentActivity;
+import com.teamspace.android.utils.Constants;
 
 import android.support.v4.app.Fragment;
 
@@ -8,7 +9,12 @@ public class EmployeeAddEditActivity extends SingleFragmentActivity {
 
 	@Override
 	protected Fragment createFragment() {
-        return new ContactsListFragment();
+        boolean editMode = getIntent().getStringExtra(Constants.EMPLOYEE_ID) != null ? true : false;
+        if (editMode) {
+            return new EmployeeAddEditFragment();
+        } else {
+            return new ContactsListFragment();
+        }
 	}
 
 }
